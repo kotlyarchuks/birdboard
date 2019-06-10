d56afc10af3dcfb8d008ab8125d98f890d9edf8b
Denis Kotlyarchuk@ <kotlyarchukd@gmail.com@>
2019-06-10 22:25:10 +0300

    Projects dashboard styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,30 +12,21 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body class="bg-gray-300">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <nav class="bg-white">
+            <div class="container mx-auto flex justify-between py-4 items-center">
+                <div>
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                </div>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
+                <div>
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
@@ -72,7 +63,7 @@
             </div>
         </nav>
 
-        <main class="py-4 container">
+        <main class="py-4 container mx-auto">
             @yield('content')
         </main>
     </div>
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,14 +1,21 @@
 @extends('layouts.app')
 @section('content')
-    <ul>
-            @forelse($projects as $project)
-            <li>
-                <a href="{{$project->path()}}">{{$project->title}}</a>
-            </li>
-            @empty
+    <div class="flex">
+        @forelse($projects as $project)
+            <div class="bg-white mr-4 p-5 w-1/4 rounded shadow" style="height:200px">
+                <h3 class="py-3 text-xl"><a href="{{$project->path()}}">{{$project->title}}</a></h3>
+
+                <div class="text-gray-600">
+                    {{\Illuminate\Support\Str::limit($project->description, 100)}}
+                </div>
+            </div>
+        @empty
+            <div>
                 No projects yet!
-            @endforelse
-    </ul>
+            </div>
+        @endforelse
+    </div>
+
     <br>
     <a href="/projects/create">Create new project</a>
 @endsection
