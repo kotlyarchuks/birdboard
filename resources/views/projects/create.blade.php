41c6336d481c4224c3a879936b03a88a991d1030
Denis Kotlyarchuk@ <kotlyarchukd@gmail.com@>
2019-06-12 19:04:50 +0300

    Styling for creating project page

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,17 +1,31 @@
 @extends('layouts.app')
 @section('content')
-<form action="/projects" method="post">
-    @csrf
-    <div>
-        <label for="title">Title:</label>
-        <input id="title" name="title" type="text" required>
+    <div class="flex justify-center">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/3" action="/projects" method="post">
+            @csrf
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="title">
+                    Title
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                       id="title" name="title" type="text" placeholder="">
+            </div>
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="description">
+                    Description
+                </label>
+                <textarea
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        id="description" name="description" placeholder="" style="min-height: 100px"></textarea>
+            </div>
+            <div class="flex items-center justify-center">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-5 rounded focus:outline-none focus:shadow-outline"
+                        type="sumbit">
+                    Create
+                </button>
+                <a href="/projects">Go back</a>
+            </div>
+        </form>
     </div>
-    <div>
-        <label for="description">Description:</label>
-        <input id="description" name="description" type="text" required>
-    </div>
-    <button type="submit">Create</button>
-</form>
 
-<a href="/projects">Go back</a>
 @endsection
