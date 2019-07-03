fda50625a66665733e9c5700145120700f271de0
Denis Kotlyarchuk@ <kotlyarchukd@gmail.com@>
2019-07-03 20:54:21 +0300

    Separate views for every activity. Styling for activity bar

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -44,9 +44,14 @@
             <div class="w-1/4">
                 @include('layouts.card')
                 <div class="card mt-3">
-                    @foreach($project->activities as $activity)
-                        <div>{{$activity->description}} - {{$activity->created_at->diffForHumans()}}</div>
-                    @endforeach
+                    @forelse($project->activities as $activity)
+                        <div class="text-sm text-muted">
+                            @include("projects.activities.{$activity->description}")
+                            - <span class="text-gray-500">{{$activity->created_at->diffForHumans(null, true)}}</span>
+                        </div>
+                    @empty
+                        <div class="text-sm text-muted">There is no activity yet</div>
+                    @endforelse
                 </div>
             </div>
         </div>
